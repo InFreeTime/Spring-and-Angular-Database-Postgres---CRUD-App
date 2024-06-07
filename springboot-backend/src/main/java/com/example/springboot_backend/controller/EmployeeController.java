90df9adde21ffb4d7316632ceb0fe6a664b958d4
Dawid <interfud@wp.pl>
2024-06-07 17:08:54 +0200

    Wczytywanie listy pracownikow z backend do frontend, dodawanie nowego pracownika - formularz

---
+++ springboot-backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.springboot_backend.controller;
+
+import com.example.springboot_backend.model.Employee;
+import com.example.springboot_backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    // create employee restapi
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+
+}
